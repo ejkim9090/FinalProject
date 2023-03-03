60f9db79ad60e3935d805a289c4cc4a99d5e1b2a
wjdcks1238 <wjdcks1238@naver.com>
2023-03-03 19:15:44 +0900

    최초업로드
    -

--- sql/시스디비에이.sql
+++
@@ -1,5 +0,0 @@
-create user scott identified by TIGER;
-grant connect, resource, create any table to scott;
-grant connect, resource, dba to scott;
-create user KH identified by KH;
-grant connect, resource, create any table to KH;
