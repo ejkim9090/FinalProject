1951d2a98c83f14fd899adaa9f2f11ccf5342100
wjdcks1238 <wjdcks1238@naver.com>
2023-03-08 15:10:16 +0900

    cloudniary api 이미지 파일 업로드 테스트

--- src/main/java/com/kh/teamproject/HomeController.java
+++ src/main/java/com/kh/teamproject/HomeController.java
@@ -1,6 +1,7 @@
 package com.kh.teamproject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
@@ -8,7 +9,9 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -28,6 +31,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
 
 
 
@@ -134,9 +141,23 @@ public class HomeController {
 			e1.printStackTrace();
 		}
 		//임시로 써본거라 지우셔도 됩니다!
-		
-		
-		
+/*		
+		//cloudinary API 테스트
+		// Configure
+		Map config = new HashMap();
+		config.put("cloud_name", "dd4rzi6aj");
+		config.put("api_key", "328149227124265");
+		config.put("api_secret", "RDJ2-M9CAkGwJxX1WjEt5EVoUgU");
+		Cloudinary cloudinary = new Cloudinary(config);
+
+
+		// Upload
+		try {
+		  cloudinary.uploader().upload("C:\\Users\\Luminite\\Pictures\\Screenshots\\marketsample.png", ObjectUtils.asMap("sample_beta", "market_sample"));
+		} catch (IOException exception) {
+		  System.out.println(exception.getMessage());
+		}
+*/		
 		
 		return "home";
 	}
