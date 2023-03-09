c3626468c6a858529845cb49ceb8cfab2ab10e41
brown46 <rlawlsxo623@naver.com>
2023-03-09 09:59:30 +0900

    경로 수정

--- src/main/java/com/kh/teamproject/board/controller/BoardController.java
+++ src/main/java/com/kh/teamproject/board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.kh.teamprojet.board;
+package com.kh.teamproject.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
